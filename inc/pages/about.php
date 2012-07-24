ad32644e0a5812e97438c555128473d57f981854
Darklg <kev.rocher@gmail.com>
2012-07-24 09:21:00 +0200

    Base v 1.1 : Centered layouts

--- inc/pages/about.php
+++ inc/pages/about.php
@@ -1,19 +1,21 @@
-<section class="block-content">
-	<h2 id="le-content">Qu'est-ce que c'est ?</h2>
-	<div class="le-content">
-		<p>
-			<strong>CSS Normalize</strong> est un framework, ou plutôt une base de départ en CSS, qui comprend :
-		</p>
-		<ul>
-			<li>Un <em>reset CSS</em> inspiré de celui de <a href="http://html5boilerplate.com/" target="_blank">HTML Boilerplate</a>.</li>
-			<li>Une feuille de style par défaut, stylisant les balises HTML les plus utilisées.</li>
-			<li>Une feuille d'objets CSS utilisés fréquemment.</li>
-			<li>Une feuille de démarrage basique.</li>
-			<li>Des modules fonctionnant indépendamment.</li>
-		</ul>
-		<h3>Sous-modules</h3>
-		<p>
-			Ces sous-modules sont des feuilles de style indépendantes, à utiliser avec les feuilles de base de CSSNormalize.
-		</p>
-	</div>
-</section>
+<div class="centered-container container-about">
+    <section>
+    	<h2 id="le-content">Qu'est-ce que c'est ?</h2>
+    	<div class="le-content">
+    		<p>
+    			<strong>CSS Normalize</strong> est un framework, ou plutôt une base de départ en CSS, qui comprend :
+    		</p>
+    		<ul>
+    			<li>Un <em>reset CSS</em> inspiré de celui de <a href="http://html5boilerplate.com/" target="_blank">HTML Boilerplate</a>.</li>
+    			<li>Une feuille de style par défaut, stylisant les balises HTML les plus utilisées.</li>
+    			<li>Une feuille d'objets CSS utilisés fréquemment.</li>
+    			<li>Une feuille de démarrage basique.</li>
+    			<li>Des modules fonctionnant indépendamment.</li>
+    		</ul>
+    		<h3>Sous-modules</h3>
+    		<p>
+    			Ces sous-modules sont des feuilles de style indépendantes, à utiliser avec les feuilles de base de CSSNormalize.
+    		</p>
+    	</div>
+    </section>
+</div>

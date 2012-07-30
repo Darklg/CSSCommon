976a7cd0d4983744e8a96c782c8491ab05fcdfcf
Darklg <kev.rocher@gmail.com>
2012-07-30 21:54:17 +0200

    Demo avec des images diverses.

--- inc/pages/responsive-layout.php
+++ inc/pages/responsive-layout.php
@@ -3,7 +3,11 @@
 	   <div class="col-main">
 	       <div class="content-container">
 		   <p>A lot of people in our industry haven't had very diverse experiences. So they don't have enough dots to connect, and they end up with very linear solutions without a broad perspective on the problem. The broader one's understanding of the human experience, the better design we will have.</p>
+		   <h3>Une image trop grande (900px)</h3>
 		   <p><img src="images/900x100-cat.jpg" alt="900x100 Cat" /></p>
+		   <p>A lot of people in our industry haven't had very diverse experiences. So they don't have enough dots to connect, and they end up with very linear solutions without a broad perspective on the problem. The broader one's understanding of the human experience, the better design we will have.</p>
+		   <h3>Une image trop grande, et qui ignore les marges (900px)</h3>
+		   <p class="large-content-area"><img src="images/900x100-cat.jpg" alt="" /></p>
 		   <p>A lot of people in our industry haven't had very diverse experiences. So they don't have enough dots to connect, and they end up with very linear solutions without a broad perspective on the problem. The broader one's understanding of the human experience, the better design we will have.</p>
 		   </div>
 	   </div>

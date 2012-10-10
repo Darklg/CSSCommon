0a5b43d6f69d505032a0b549087064c41ebb6256
Darklg <kev.rocher@gmail.com>
2012-10-10 16:29:03 +0200

    Normalize Common isolé + maj links css

---
+++ inc/pages/normalize-common.php
@@ -0,0 +1,35 @@
+<section class="block-content" id="bc-common">
+    <h2 id="normalize-common">&Eacute;léments récurrents</h2>
+    <div>
+        <h3>Barre d’avancement :</h3>
+        <span class="lvl-barre">
+            <span class="bar" style="width:30%;"></span>
+        </span>
+        <h3>Effet Tiroir</h3>
+        <div class="wdrw">
+            <p>
+                <a href="#">Survolez-moi !</a>
+            </p>
+            <div class="submenu" >
+                <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed non risus. Suspendisse lectus tortor, dignissim sit amet, adipiscing nec, ultricies sed, dolor. Cras elementum ultrices diam. Maecenas ligula massa, varius a, semper congue, euismod non, mi. Proin porttitor, orci nec nonummy molestie, enim est eleifend mi, non fermentum diam nisl sit amet erat. Duis semper. Duis arcu massa, scelerisque vitae, consequat in, pretium a, enim. Pellentesque congue. Ut in risus volutpat libero pharetra tempor. Cras vestibulum bibendum augue. Praesent egestas leo in pede.</p>
+            </div>
+        </div>
+        <h3>Coupure du texte en CSS au delà d'une certaine taille</h3>
+        <p class="non_depassement">Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed non risus. Suspendisse lectus tortor, dignissim sit amet, adipiscing nec, ultricies sed, dolor. Cras elementum ultrices diam. Maecenas ligula massa, varius a, semper congue, euismod non, mi. Proin porttitor, orci nec nonummy molestie, enim est eleifend mi, non fermentum diam nisl sit amet erat. Duis semper. Duis arcu massa, scelerisque vitae, consequat in, pretium a, enim. Pellentesque congue. Ut in risus volutpat libero pharetra tempor. Cras vestibulum bibendum augue. Praesent egestas leo in pede.</p>
+        <h3>
+            Block media (inspiré par
+            <a href="http://www.stubbornella.org/content/2010/06/25/the-media-object-saves-hundreds-of-lines-of-code/">@Stubbornella</a>
+            )
+        </h3>
+        <div class="bmedia">
+            <a href="#">
+                <img src="http://placekitten.com/50/50" alt="" />
+            </a>
+            <div class="bm-cont">
+                <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed non risus. Suspendisse lectus tortor, dignissim sit amet, adipiscing nec, ultricies sed, dolor. Cras elementum ultrices diam. Maecenas ligula massa, varius a, semper congue, euismod non, mi. Proin porttitor, orci nec nonummy molestie, enim est eleifend mi, non fermentum diam nisl sit amet erat. Duis semper. Duis arcu massa, scelerisque vitae, consequat in, pretium a, enim. Pellentesque congue. Ut in risus volutpat libero pharetra tempor. Cras vestibulum bibendum augue. Praesent egestas leo in pede.</p>
+            </div>
+        </div>
+        <h3>Remplacement d'image</h3>
+        <span class="ir demo-ir"></span>
+    </div>
+</section>
--- index.php
+++ index.php
@@ -3,32 +3,33 @@
     <head>
         <title>CSS Normalize - Framework CSS</title>
         <meta charset="UTF-8" />
-        
+
         <!-- Magie des navigateurs -->
         <meta name="viewport" content="width=1020" />
         <meta http-equiv="X-UA-Compatible" content="IE=edge,chrome=1">
-        
+
         <!-- Feuilles Normalize -->
-        <link rel="stylesheet" href="css/main.css?v=201209101003" type="text/css" />
+        <link rel="stylesheet" href="css/main.css?v=201210101628" type="text/css" />
 
         <!-- Scripts -->
         <!--[if lt IE 9]><script src="js/html5.js"></script><![endif]-->
         <script src="js/functions.js"></script>
     </head>
 
-    <!--[if lt IE 7 ]><body class="is_ie6 lt_ie7 lt_ie8 lt_ie9 lt_ie10"><![endif]--> 
-    <!--[if IE 7 ]><body class="is_ie7 lt_ie8 lt_ie9 lt_ie10"><![endif]--> 
-    <!--[if IE 8 ]><body class="is_ie8 lt_ie9 lt_ie10"><![endif]--> 
-    <!--[if IE 9 ]><body class="is_ie9 lt_ie10"><![endif]--> 
-    <!--[if gt IE 9]><body class="is_ie10"><![endif]--> 
+    <!--[if lt IE 7 ]><body class="is_ie6 lt_ie7 lt_ie8 lt_ie9 lt_ie10"><![endif]-->
+    <!--[if IE 7 ]><body class="is_ie7 lt_ie8 lt_ie9 lt_ie10"><![endif]-->
+    <!--[if IE 8 ]><body class="is_ie8 lt_ie9 lt_ie10"><![endif]-->
+    <!--[if IE 9 ]><body class="is_ie9 lt_ie10"><![endif]-->
+    <!--[if gt IE 9]><body class="is_ie10"><![endif]-->
     <!--[if !IE]><!--> <body class=""><!--<![endif]-->
     <div id="main-container">
-        
+
         <header id="header">
             <h1>CSS Normalize - Framework CSS</h1>
         </header>
 
         <?php include dirname(__FILE__).'/inc/pages/about.php'; ?>
+        <?php include dirname(__FILE__).'/inc/pages/normalize-common.php'; ?>
         <?php include dirname(__FILE__).'/inc/pages/normalize-forms.php'; ?>
         <?php include dirname(__FILE__).'/inc/pages/normalize-tables.php'; ?>
         <?php include dirname(__FILE__).'/inc/pages/normalize-grid.php'; ?>
@@ -39,11 +40,12 @@
         <?php include dirname(__FILE__).'/inc/pages/normalize-tabs.php'; ?>
 
     </div>
-    
+
     <footer class="fixed-footer">
     <nav>
         <ul>
             <li><a href="#le-content">Contenu</a></li>
+            <li><a href="#normalize-common">Récurrents</a></li>
             <li><a href="#normalize-forms">Formulaires</a></li>
             <li><a href="#normalize-tables">Tables</a></li>
             <li><a href="#normalize-grid">Grilles</a></li>
@@ -57,8 +59,8 @@
         </ul>
     </nav>
     </footer>
-    
+
     <a href="http://github.com/darklg/CSSNormalize" id="fork_me"><img src="images/fork_me.png" alt="Fork me on GitHub" /></a>
-    <script src="js/events.js"></script>    
+    <script src="js/events.js"></script>
     </body>
 </html>

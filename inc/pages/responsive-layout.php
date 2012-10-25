03c984e9b073f65ecbf11f3fb67e187be2c0e085
Darklg <kev.rocher@gmail.com>
2012-10-25 19:02:47 +0200

    Layout v 1.4 : Classes de visibilité

--- inc/pages/responsive-layout.php
+++ inc/pages/responsive-layout.php
@@ -1,38 +1,64 @@
 <section class="block-content">
-	<div class="cssn-lay clay-resp">
-	   <div class="col-main">
-	       <div class="content-container">
-		   <p>A lot of people in our industry haven't had very diverse experiences. So they don't have enough dots to connect, and they end up with very linear solutions without a broad perspective on the problem. The broader one's understanding of the human experience, the better design we will have.</p>
-		   <h3>Une image trop grande (900px)</h3>
-		   <p><img src="images/900x100-cat.jpg" alt="900x100 Cat" /></p>
-		   <p>A lot of people in our industry haven't had very diverse experiences. So they don't have enough dots to connect, and they end up with very linear solutions without a broad perspective on the problem. The broader one's understanding of the human experience, the better design we will have.</p>
-		   <h3>Une image trop grande, et qui ignore les marges (900px)</h3>
-		   <p class="large-content-area"><img src="images/900x100-cat.jpg" alt="" /></p>
-		   <p>A lot of people in our industry haven't had very diverse experiences. So they don't have enough dots to connect, and they end up with very linear solutions without a broad perspective on the problem. The broader one's understanding of the human experience, the better design we will have.</p>
-		   </div>
-	   </div>
-	   <div class="col-side">
-	       <ul class="liste-widgets content-container">
-               <li>
-                   <h3>A Widget</h3>
-                   <ul>
-                      <li><a href="#">lorem ipsum</a></li>
-                      <li><a href="#">dolor sit amet</a></li>
-                  </ul>
-               </li>
-               <li>
-                   <h3>Another Widget</h3>
-                   <ul>
-                       <li>ipsum facto</li>
-                       <li>sit dolor amet</li>
-                   </ul>
-               </li>
-               <li>
+    <div class="cssn-lay clay-resp">
+        <div class="col-main">
+            <div class="content-container">
+                <p>A lot of people in our industry haven't had very diverse experiences. So they don't have enough dots to connect, and they end up with very linear solutions without a broad perspective on the problem. The broader one's understanding of the human experience, the better design we will have.</p>
+                <h3>Une image trop grande (900px)</h3>
+                <p>
+                    <img src="images/900x100-cat.jpg" alt="900x100 Cat" />
+                </p>
+                <p>A lot of people in our industry haven't had very diverse experiences. So they don't have enough dots to connect, and they end up with very linear solutions without a broad perspective on the problem. The broader one's understanding of the human experience, the better design we will have.</p>
+                <h3>Une image trop grande, et qui ignore les marges (900px)</h3>
+                <p class="large-content-area">
+                    <img src="images/900x100-cat.jpg" alt="" />
+                </p>
+                <p>A lot of people in our industry haven't had very diverse experiences. So they don't have enough dots to connect, and they end up with very linear solutions without a broad perspective on the problem. The broader one's understanding of the human experience, the better design we will have.</p>
+            </div>
+        </div>
+        <div class="col-side">
+            <ul class="liste-widgets content-container">
+                <li>
+                    <h3>A Widget</h3>
+                    <ul>
+                        <li>
+                            <a href="#">lorem ipsum</a>
+                        </li>
+                        <li>
+                            <a href="#">dolor sit amet</a>
+                        </li>
+                    </ul>
+                </li>
+                <li>
+                    <h3>Another Widget</h3>
+                    <ul>
+                        <li>ipsum facto</li>
+                        <li>sit dolor amet</li>
+                    </ul>
+                </li>
+                <li>
                     <h3>Text Widget</h3>
-                    <p>The world needs dreamers and the world needs doers. But above all, the world needs dreamers who do. </p>
-               </li>
-           </ul>
-		   
-	   </div>
-	</div>
+                    <p>The world needs dreamers and the world needs doers. But above all, the world needs dreamers who do.</p>
+                </li>
+            </ul>
+        </div>
+    </div>
+    <h3>Classes de visibilité</h3>
+    <div class="visible-only-phone">
+        <pre>.visible-only-phone {}</pre>
+    </div>
+    <div class="visible-only-tablet">
+        <pre>.visible-only-tablet {}</pre>
+    </div>
+    <div class="visible-only-full">
+        <pre>.visible-only-full {}</pre>
+    </div>
+    <div class="hidden-on-phone">
+        <pre>.hidden-on-phone {}</pre>
+    </div>
+    <div class="hidden-on-tablet">
+        <pre>.hidden-on-tablet {}</pre>
+    </div>
+    <div class="hidden-on-full">
+        <pre>.hidden-on-full {}</pre>
+    </div>
 </section>

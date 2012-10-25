48eb4e2e78c7fb59abd9111f0028aa35285ddb59
Darklg <kev.rocher@gmail.com>
2012-10-25 18:39:45 +0200

    Demo v 1.1.1 : Prise en compte des nouvelles grid

--- responsive.php
+++ responsive.php
@@ -9,7 +9,7 @@
         <meta http-equiv="X-UA-Compatible" content="IE=edge,chrome=1">
 
         <!-- Feuilles Normalize -->
-        <link rel="stylesheet" href="css/main.css?v=201210230949" type="text/css" />
+        <link rel="stylesheet" href="css/main.css?v=201210251839" type="text/css" />
 
         <!-- Scripts -->
         <!--[if lt IE 9]><script src="js/html5.js"></script><![endif]-->

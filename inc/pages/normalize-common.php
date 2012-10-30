82eae31f4f61c618fb446666986ed20de07536f5
Darklg <kev.rocher@gmail.com>
2012-10-30 14:11:04 +0100

    Common v 1.6.3 : Mise en place de la nouvelle méthode de transition

--- inc/pages/normalize-common.php
+++ inc/pages/normalize-common.php
@@ -10,7 +10,7 @@
             <p>
                 <a href="#">Survolez-moi !</a>
             </p>
-            <div class="submenu" >
+            <div class="submenu trans-opa">
                 <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed non risus. Suspendisse lectus tortor, dignissim sit amet, adipiscing nec, ultricies sed, dolor. Cras elementum ultrices diam. Maecenas ligula massa, varius a, semper congue, euismod non, mi. Proin porttitor, orci nec nonummy molestie, enim est eleifend mi, non fermentum diam nisl sit amet erat. Duis semper. Duis arcu massa, scelerisque vitae, consequat in, pretium a, enim. Pellentesque congue. Ut in risus volutpat libero pharetra tempor. Cras vestibulum bibendum augue. Praesent egestas leo in pede.</p>
             </div>
         </div>

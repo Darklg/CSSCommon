07495ca92e9b3dd5d093873575657943eb9e0233
Darklg <kev.rocher@gmail.com>
2012-10-30 14:15:15 +0100

    Modeles v 1.3.7 : Bugfix sur menu + Nouvelle méthode de transition

--- inc/pages/normalize-modeles.php
+++ inc/pages/normalize-modeles.php
@@ -1,7 +1,7 @@
 <section class="block-content">
 	<h2 id="normalize-modeles">Mod&egrave;les</h2>
 	<p>Mod&egrave;les d&#x27;objets r&eacute;currents dans les int&eacute;grations.</p>
-	
+
 	<h3>Pagination</h3>
 	<ul class="cssn_pagination">
 		<li><a href="#" class="prev">&lsaquo; Pr&eacute;c&eacute;dent</a></li>
@@ -14,7 +14,7 @@
 		</li>
 		<li><a href="#" class="next">Suivant &rsaquo;</a></li>
 	</ul>
-	
+
 	<h3>Pagination centrée</h3>
 	<ul class="cssn_pagination cssn_pagination_center">
 		<li><a href="#" class="prev">&lsaquo; Pr&eacute;c&eacute;dent</a></li>
@@ -27,7 +27,7 @@
 		</li>
 		<li><a href="#" class="next">Suivant &rsaquo;</a></li>
 	</ul>
-	
+
 	<h3>Menu Principal avec sous-menu</h3>
 	<nav class="cssn_mainnav">
 		<ul class="cssn_mainmenu">
@@ -35,7 +35,7 @@
 			<li><a href="#">Lien normal</a></li>
 			<li class="withsubmenu">
 				<a href="#">[+] Produits</a>
-				<div class="submenu">
+				<div class="submenu trans-opa">
 					<ul>
 						<li><a href="#">Template HTML</a></li>
 						<li><a href="#">Template WordPress</a></li>
@@ -48,8 +48,8 @@
 			<li class="current"><a href="#">Lien actuel</a></li>
 		</ul>
 	</nav>
-	
-	
+
+
 	<h3>Menu Principal avec sous-menu &quot;riche&quot; de largeur fixe</h3>
 	<nav class="cssn_mainnav">
 		<ul class="cssn_mainmenu">
@@ -57,7 +57,7 @@
 			<li><a href="#">Lien normal</a></li>
 			<li class="richsubmenu2">
 				<a href="#">[+] Produits</a>
-				<div class="submenu">
+				<div class="submenu trans-opa">
 					<div class="subcol">
 						<h3>Ma catégorie</h3>
 						<ul>
@@ -90,7 +90,7 @@
 			<li class="current"><a href="#">Lien actuel</a></li>
 		</ul>
 	</nav>
-	
+
 	<h3>Menu Principal avec sous-menu &quot;riche&quot; 100%</h3>
 	<nav class="cssn_mainnav">
 		<ul class="cssn_mainmenu">
@@ -98,7 +98,7 @@
 			<li><a href="#">Lien normal</a></li>
 			<li class="richsubmenu">
 				<a href="#">[+] Produits</a>
-				<div class="submenu">
+				<div class="submenu trans-opa">
 					<div class="subcol">
 						<h3>Ma catégorie</h3>
 						<ul>
@@ -131,7 +131,7 @@
 			<li class="current"><a href="#">Lien actuel</a></li>
 		</ul>
 	</nav>
-	
+
 	<h3>Menu Principal avec Social links</h3>
 	<nav class="cssn_mainnav">
 		<ul class="cssn_mainmenuplus">
@@ -145,8 +145,8 @@
 			<li class="current"><a href="#">Lien actuel</a></li>
 		</ul>
 	</nav>
-	
-	
+
+
 	<h3>Fil d'ariane</h3>
 	<ul class="cssn_ariane">
 		<li itemscope itemtype="http://data-vocabulary.org/Breadcrumb">

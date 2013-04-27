2db83e66cc633aa17660c1c3e692aee9ea73a6f6
Darklg <kev.rocher@gmail.com>
2013-04-27 19:07:12 +0200

    Demo - Description text for gallery

--- inc/pages/cssc-gallery.php
+++ inc/pages/cssc-gallery.php
@@ -1,5 +1,6 @@
 <section class="block-content">
     <h2 id="cssc-gallery">Galerie</h2>
+    <p>Une marge se crée automatiquement entre les éléments, mais le dernier élément d'une ligne est tout de même calé à droite, quel que soit le nombre d'éléments par ligne.</p>
     <ul class="subfloat sf_100_20">
         <li><a href="#"><img src="images/100x100-black.png" alt="" /></a></li>
         <li><a href="#"><img src="images/100x100-black.png" alt="" /></a></li>

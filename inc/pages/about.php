78df1b91cfe91c31d42a0f8be5c5e7e32ea07768
Darklg <kev.rocher@gmail.com>
2013-06-10 22:26:38 +0200

    Demo v 1.1.11 : Fixes & better "about" section

--- inc/pages/about.php
+++ inc/pages/about.php
@@ -1,33 +1,54 @@
 <div class="centered-container container-about">
     <section>
-        <h2 id="le-content">Qu'est-ce que c'est ?</h2>
         <div class="le-content">
-            <p>
-                <strong>CSS Common</strong> est un framework, ou plutôt une base de départ en CSS, qui comprend :
-            </p>
-            <ul>
-                <li>Une feuille de style <strong>par défaut</strong>, réinitialisant et stylisant les balises <abbr title="Hypertext Markup Language">HTML</abbr> les plus utilisées.</li>
-                <li>Une feuille d'<strong>objets <abbr title="Cascading Style Sheets">CSS</abbr> utilisés fréquemment</strong>.</li>
-                <li>Des modules à rajouter pour chaque besoin.</li>
-            </ul>
-            <h3>Modules</h3>
-            <p>
-                Ces modules sont des feuilles de style indépendantes, à utiliser avec les feuilles de base de CSSCommon.
-            </p>
-            <h3>Responsive web design</h3>
-            <p>
-                Un exemple simplifié de <a href="responsive.php">layout Responsive</a> avec CSSCommon.
-            </p>
-            <h3>Pourquoi ?</h3>
-            <blockquote lang="en">
-                <p>The miracle is this: the more we share the more we have.</p>
-                <em><cite>Leonard Nimoy</cite></em>
-            </blockquote>
-            <h3>Pour démarrer</h3>
-            <p>
-                <a target="_blank" href="https://github.com/Darklg/CSSCommon" class="cssc-button le-medium-button">Forkez le projet sur Github</a>
-                <a target="_blank" href="https://github.com/Darklg/CSSCommon/archive/master.zip" class="cssc-button le-medium-button">Téléchargez une archive .zip</a>
-            </p>
+            <div class="cssc-grid gri-940-24-20">
+                <div class="col-12">
+                    <h2 id="le-content">Qu'est-ce que c'est ?</h2>
+                    <p>
+                        <strong>CSS Common</strong>
+                        est un framework, ou plutôt une base de départ en CSS, qui comprend :
+                    </p>
+                    <ul>
+                        <li>
+                            Une feuille
+                            <strong>par défaut</strong>
+                            , réinitialisant et stylisant des balises
+                            <abbr title="Hypertext Markup Language">HTML</abbr>
+                            courantes.
+                        </li>
+                        <li>
+                            Une feuille d'
+                            <strong>
+                                objets
+                                <abbr title="Cascading Style Sheets">CSS</abbr>
+                                utilisés fréquemment.
+                            </strong>
+                        </li>
+                        <li>Des modules supplémentaires pour chaque besoin.</li>
+                    </ul>
+                </div>
+                <div class="col-12">
+                    <h3>Responsive web design</h3>
+                    <p>
+                        Un exemple simplifié de
+                        <a href="responsive.php">layout Responsive</a>
+                        avec CSSCommon.
+                    </p>
+                    <h3>Pourquoi ?</h3>
+                    <blockquote lang="en">
+                        <p>The miracle is this: the more we share the more we have.</p>
+                        <em>
+                            <cite>Leonard Nimoy</cite>
+                        </em>
+                    </blockquote>
+                </div>
+            </div>
+        </div>
+        <div class="cssc-button-container">
+            <a target="_blank" href="https://github.com/Darklg/CSSCommon" class="cssc-button cssc-button-double">
+                <strong>Parcourez le projet sur Github</strong>
+                <span>Star it, Clone it, Fork it, Download it !</span>
+            </a>
         </div>
     </section>
 </div>

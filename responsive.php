a7439b6012689f2c40dfdaed9db6fa5fe7ab2bf6
Darklg <kev.rocher@gmail.com>
2013-06-12 08:27:53 +0200

    Demo v 1.1.12 : Hiding "Fork Me" if viewport is too small

--- responsive.php
+++ responsive.php
@@ -33,7 +33,7 @@
 
     </div>
 
-    <a href="http://github.com/darklg/CSSCommon" id="fork_me"><img src="images/fork_me.png" alt="Fork me on GitHub" /></a>
+    <a href="http://github.com/darklg/CSSCommon" class="fork_me trans-all"><img src="images/fork_me.png" alt="Fork me on GitHub" /></a>
     <script src="js/events.js"></script>
     </body>
 </html>

59f9fa2c21324c0758cf2c2dbbb0c0f13584c9b3
Darklg <kev.rocher@gmail.com>
2013-06-24 18:22:44 +0200

    Default v 2.6 : Reorganization / new styles / bugfixes

--- inc/blocks/html.php
+++ inc/blocks/html.php
@@ -1,5 +1,10 @@
 <section class="block-content centered-container"><div>
     <h2>Style HTML</h2>
+    <p>
+        Le container <code>.cssc-content</code> permet de styliser des listes, d'activer une taille de typo de base, de faire flotter des images. <br />
+        Il est utilisé par exemple dans le contenu d'un article, dans une description de fiche produit, ...<br />
+        Les autres styles HTML sont disponibles à tous les niveaux du DOM.
+    </p>
     <div class="cssc-grid gri-940-24-20">
         <div class="col-8">
             <h3>Lists</h3>
@@ -25,15 +30,34 @@
         <div class="col-8">
             <h3>Content</h3>
             <div class="cssc-content">
-                <p>Lorem ipsum dolor sit amet, <abbr title="consectetur">csc</abbr> adipiscing elit. Sed non <strong>risus</strong>. Suspendisse lectus tortor, <a href="#">dignissim</a> sit amet, adipiscing nec, ultricies sed, dolor. Cras el<em></em>ementum ultrices diam. <img src="images/12x10-blue.gif" alt="" /> Maecenas ligula massa, varius a, semper congue, euismod non, mi. Proin porttitor, <a href="#">orci nec <strong>nonummy</strong> molestie</a>, enim est eleifend mi, non fermentum diam nisl sit amet erat. </p>
+                <p>
+                    <img src="images/50x50-green.gif" class="alignleft" alt="" />
+                    Lorem ipsum dolor sit amet, <abbr title="abbreviation">abbr</abbr> adipiscing elit. Sed non <strong>strong</strong>.
+                    <img src="images/12x10-blue.gif" alt="img" /> Suspendisse lectus tortor, <a href="#">link</a> sit amet, adipiscing nec, ultricies sed, dolor.
+                    Cras <em>em</em>ementum ultrices diam.
+                </p>
+                <p>
+                    Maecenas ligula<sup>sup</sup>, varius a, semper congue, euismod non, mi. Proin porttitor<sub>sub</sub>,
+                    <a href="#">a <strong>strong</strong> link</a>,
+                    enim est <del>del</del> <ins>ins</ins>, non <small>small</small> diam nisl sit amet erat.
+                </p>
+                <hr />
             </div>
         </div>
         <div class="col-8">
-            <h1>Title lvl. 1</h1>
-            <h2>Title lvl. 2</h2>
-            <h3>Title lvl. 3</h3>
-            <h4>Title lvl. 4</h4>
-            <h5>Title lvl. 5</h5>
+            <div class="cssc-block">
+                <h1>Title lvl. 1</h1>
+                <h2>Title lvl. 2</h2>
+                <h3>Title lvl. 3</h3>
+                <h4>Title lvl. 4</h4>
+                <h5>Title lvl. 5</h5>
+            </div>
+            <blockquote>
+                <p>Contenu d'une citation</p>
+                <em>
+                    <cite>Auteur</cite>
+                </em>
+            </blockquote>
         </div>
     </div>
 </div></section>

04af8b7502af00a9ad24febcbab7d129bcf43c3a
Darklg <kev.rocher@gmail.com>
2013-07-24 19:24:29 +0200

    Common v 1.9.2 : Tooltips direction & fixes

--- inc/blocks/common.php
+++ inc/blocks/common.php
@@ -13,15 +13,18 @@
                 <p class="non-depassement">Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed non risus. Suspendisse lectus tortor, dignissim sit amet, adipiscing nec, ultricies sed, dolor. Cras elementum ultrices diam. Maecenas ligula massa, varius a, semper congue, euismod non, mi. Proin porttitor, orci nec nonummy molestie, enim est eleifend mi, non fermentum diam nisl sit amet erat. Duis semper. Duis arcu massa, scelerisque vitae, consequat in, pretium a, enim. Pellentesque congue. Ut in risus volutpat libero pharetra tempor. Cras vestibulum bibendum augue. Praesent egestas leo in pede.</p>
             </div>
             <div class="col-8">
-                <h3>Effet Tiroir</h3>
-                <div class="wdrw">
-                    <p>
-                        <a href="#">Survolez-moi !</a>
-                    </p>
-                    <div class="submenu trans-opa">
-                        <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed non risus. Suspendisse lectus tortor, dignissim sit amet, adipiscing nec, ultricies sed, dolor.</p>
-                    </div>
-                </div>
+                <h3>Infobulle</h3>
+                <p>
+                    <a href="#" class="tooltip">
+                        Infobulle
+                        <span class="tooltip-bubble trans-opa">Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed non risus. </span>
+                    </a>
+                    •
+                    <a href="#" class="tooltip tooltip-bottom">
+                        Dessous
+                        <span class="tooltip-bubble trans-opa">Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed non risus. </span>
+                    </a>
+                </p>
             </div>
         </div>
         <div class="cssc-grid gri-940-24-20">
@@ -43,15 +46,16 @@
                 <span class="ir demo-ir"></span>
             </div>
             <div class="col-8">
-                <h3>Infobulle</h3>
-                <p>
-                    <a href="#" class="tooltip">
-                        Infobulle au survol
-                        <span class="tooltip-bubble trans-opa">Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed non risus. </span>
-                    </a>
-                </p>
+                <h3>Effet Tiroir</h3>
+                <div class="wdrw">
+                    <p>
+                        <a href="#">Survolez-moi !</a>
+                    </p>
+                    <div class="submenu trans-opa">
+                        <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed non risus. Suspendisse lectus tortor, dignissim sit amet, adipiscing nec, ultricies sed, dolor.</p>
+                    </div>
+                </div>
             </div>
-
         </div>
     </div>
 </div></section>

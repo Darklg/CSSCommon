fe03f1ac0de34b105f258e5d92254145c64e9e34
Darklg <kev.rocher@gmail.com>
2013-09-12 13:48:35 +0200

    Effects v 0.2 : Slide on load effect & Better presentation

--- inc/pages/cssc-effects.php
+++ inc/pages/cssc-effects.php
@@ -1,12 +1,28 @@
 <section class="block-content centered-container">
     <div>
+        <div class="bkch-aside">
+            Module :
+            <code>cssc-effects.css</code>
+        </div>
+        <h2>Effects</h2>
+        <p>A Library of simple effects using CSS.</p>
+    </div>
+</section>
+<section class="block-content centered-container">
+    <div>
         <h2>Hover effects</h2>
         <h3>Inverted :hover</h3>
-        <ul class="cssc-inverted-hover">
+        <ul class="cssc-inverted-hover cssc-block">
             <li><a href="#">lorem ipsum</a></li>
             <li><a href="#">dolor sit amet</a></li>
             <li><a href="#">consectetur adipiscing elit</a></li>
         </ul>
+        <p><a href="#demo-cssc-inverted-hover" class="cssc-button show-html-button">Afficher HTML</a></p>
+        <pre contenteditable id="demo-cssc-inverted-hover">&lt;ul class=&quot;cssc-inverted-hover&quot;&gt;
+    &lt;li&gt;&lt;a href=&quot;#&quot;&gt;lorem ipsum&lt;/a&gt;&lt;/li&gt;
+    &lt;li&gt;&lt;a href=&quot;#&quot;&gt;dolor sit amet&lt;/a&gt;&lt;/li&gt;
+    &lt;li&gt;&lt;a href=&quot;#&quot;&gt;consectetur adipiscing elit&lt;/a&gt;&lt;/li&gt;
+&lt;/ul&gt;</pre>
     </div>
 </section>
 <section class="block-content centered-container">
@@ -19,5 +35,26 @@
             <img class="animate-me" src="http://placehold.it/100x100" alt="" />
             <img class="animate-me" src="http://placehold.it/100x100" alt="" />
         </p>
+        <p><a href="#demo-appear-load" class="cssc-button show-html-button">Afficher HTML</a></p>
+        <pre contenteditable id="demo-appear-load">&lt;p class=&quot;cssc-effects-appear&quot;&gt;
+    &lt;img class=&quot;animate-me&quot; src=&quot;http://placehold.it/100x100&quot; alt=&quot;&quot; /&gt;
+    &lt;img class=&quot;animate-me&quot; src=&quot;http://placehold.it/100x100&quot; alt=&quot;&quot; /&gt;
+    &lt;img class=&quot;animate-me&quot; src=&quot;http://placehold.it/100x100&quot; alt=&quot;&quot; /&gt;
+    &lt;img class=&quot;animate-me&quot; src=&quot;http://placehold.it/100x100&quot; alt=&quot;&quot; /&gt;
+&lt;/p&gt;</pre>
+        <h3>Slide on load</h3>
+        <p class="cssc-effects-slide">
+            <img class="animate-me" src="http://placehold.it/100x100/000" alt="" />
+            <img class="animate-me" src="http://placehold.it/100x100/000" alt="" />
+            <img class="animate-me" src="http://placehold.it/100x100/000" alt="" />
+            <img class="animate-me" src="http://placehold.it/100x100/000" alt="" />
+        </p>
+        <p><a href="#demo-slide-load" class="cssc-button show-html-button">Afficher HTML</a></p>
+        <pre contenteditable id="demo-slide-load">&lt;p class=&quot;cssc-effects-slide&quot;&gt;
+    &lt;img class=&quot;animate-me&quot; src=&quot;http://placehold.it/100x100&quot; alt=&quot;&quot; /&gt;
+    &lt;img class=&quot;animate-me&quot; src=&quot;http://placehold.it/100x100&quot; alt=&quot;&quot; /&gt;
+    &lt;img class=&quot;animate-me&quot; src=&quot;http://placehold.it/100x100&quot; alt=&quot;&quot; /&gt;
+    &lt;img class=&quot;animate-me&quot; src=&quot;http://placehold.it/100x100&quot; alt=&quot;&quot; /&gt;
+&lt;/p&gt;</pre>
     </div>
 </section>

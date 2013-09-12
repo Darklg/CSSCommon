ea5883dbeade23039b6f55a69d0a09e4880f2eab
Darklg <kev.rocher@gmail.com>
2013-09-12 08:52:55 +0200

    Effects v 0.1 : hover & load effects

---
+++ inc/pages/cssc-effects.php
@@ -0,0 +1,23 @@
+<section class="block-content centered-container">
+    <div>
+        <h2>Hover effects</h2>
+        <h3>Inverted :hover</h3>
+        <ul class="cssc-inverted-hover">
+            <li><a href="#">lorem ipsum</a></li>
+            <li><a href="#">dolor sit amet</a></li>
+            <li><a href="#">consectetur adipiscing elit</a></li>
+        </ul>
+    </div>
+</section>
+<section class="block-content centered-container">
+    <div>
+        <h2>Load effects</h2>
+        <h3>Appear on load</h3>
+        <p class="cssc-effects-appear">
+            <img class="animate-me" src="http://placehold.it/100x100" alt="" />
+            <img class="animate-me" src="http://placehold.it/100x100" alt="" />
+            <img class="animate-me" src="http://placehold.it/100x100" alt="" />
+            <img class="animate-me" src="http://placehold.it/100x100" alt="" />
+        </p>
+    </div>
+</section>
